ee138de5e3e781a6af794ff3494dddb2bccc834a
Hector-bit <hectormartinez.2978@gmail.com>
2020-02-24 00:01:27 -0800

    adding cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import northwestpic from './northwestforest.png';
 import './App.css';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import MediaCard from './components/projectCards';
 
 function App() {
+  let sectionStyle = {
+    width: "100%",
+    height: "400px",
+    border: 'solid red 3px',
+    backgroundColor: 'black',
+    backgroundImage: northwestpic
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='header'>
+        <div>Hector Martinez</div>
+        <a className='git' href='https://github.com/Hector-bit/'><GitHubIcon></GitHubIcon></a>
+        <a className='link' href='https://www.linkedin.com/in/hector-martinez-8a2640193/'><LinkedInIcon/></a>
+      </section>
+      <section className='top-section' styles={sectionStyle}>
+        <div>Projects</div>
+        <MediaCard></MediaCard>
+      </section>
     </div>
   );
 }
