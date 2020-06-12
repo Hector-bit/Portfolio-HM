fa0d7c28eaeb0d5cc82fcacb0387c961196a3232
Hector-bit <hectormartinez.2978@gmail.com>
2020-06-12 05:12:16 -0700

    fixed linkedin link

--- src/components/contactInfo.js
+++ src/components/contactInfo.js
@@ -13,7 +13,7 @@ function ContactInfo () {
         <section className='contact'>
             <h2>Contact Info:</h2>
             <h3>Email: hectormartinez.2978@gmail.com</h3>
-            <h3>Linkedin: <a href='https://www.linkedin.com/in/hector-martinez-8a2640193/'>Hector martinez</a></h3>
+            <h3>Linkedin: <a href='https://www.linkedin.com/in/m-hector/'>Hector martinez</a></h3>
             <h3>GitHub: <a href='https://github.com/Hector-bit/'>Hector-bit</a></h3>
         </section>
     )
