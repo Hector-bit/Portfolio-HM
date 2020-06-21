88cd9407ed54548dbb05f20232aba61ac8cc50fe
Hector-bit <hectormartinez.2978@gmail.com>
2020-06-21 10:03:30 -0700

    changed link

--- src/components/contactInfo.js
+++ src/components/contactInfo.js
@@ -5,8 +5,8 @@ function ContactInfo () {
         <section className='contact'>
             <h2>Contact Info:</h2>
             <h3>Email: hectormartinez.2978@gmail.com</h3>
-            <h3>Linkedin: <a href='https://www.linkedin.com/in/m-hector/'>Hector martinez</a></h3>
-            <h3>GitHub: <a href='https://github.com/Hector-bit/'>Hector-bit</a></h3>
+            <h3>Linkedin: <a href='https://www.linkedin.com/in/m-hector/'>https://www.linkedin.com/in/m-hector/</a></h3>
+            <h3>GitHub: <a href='https://github.com/Hector-bit/'>https://github.com/Hector-bit/</a></h3>
         </section>
     )
 }
