fe4e9f6d6dc34ae1669344ba70bafaffb41ba191
Hector-bit <hectormartinez.2978@gmail.com>
2020-09-17 15:03:39 -0700

    added a inmoov robot as another project to showcase

---
+++ src/components/projects/Inmoov.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import temp from '../../images/cool-background.png';
+import InmoovModal from './InmoovModal';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    margin: '1%',
+    padding: '1%'
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function MacroCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <a href="#">
+          <CardMedia
+          className={classes.media}
+          image={temp}
+          title="Inmoov Robot"/>
+        </a>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Inmoov Robot
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Robot project to learn about applied computer science and engineering.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <InmoovModal/>
+      </CardActions>
+    </Card>
+  );
+}
