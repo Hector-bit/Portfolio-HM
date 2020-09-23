7af406413e661b8023e7132692d8865f417be65c
Hector Martinez <hectormartinez.2978@gmail.com>
2020-09-22 23:41:45 -0700

    inmoov robot picture

--- src/components/projects/Inmoov.js
+++ src/components/projects/Inmoov.js
@@ -6,7 +6,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import temp from '../../images/cool-background.png';
+import inmoovpicture from '../../images/inmoov.png';
 import InmoovModal from './InmoovModal';
 
 const useStyles = makeStyles({
@@ -29,7 +29,7 @@ export default function MacroCard() {
         {/* <a href="#"> */}
           <CardMedia
           className={classes.media}
-          image={temp}
+          image={inmoovpicture}
           title="Inmoov Robot"/>
         {/* </a> */}
         <CardContent>
