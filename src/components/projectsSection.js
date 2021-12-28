37eb90a9ea08b9b15875f2ddb20f9dfdc54ce61c
Hector-bit <hectormartinez.2978@gmail.com>
2021-12-28 06:03:08 -0800

    removed inmoov modal

--- src/components/projectsSection.js
+++ src/components/projectsSection.js
@@ -15,7 +15,6 @@ function ProjectsSection() {
                 <MacroCard/>
                 {/* <PortfolioWebsite/> */}
                 <BorderTownRes/>
-                <Inmoov/>
                 <AndroidGame/>
             </div>
         </section>
