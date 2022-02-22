91cd14077d850b97e0a2d2bdbf41dc880cba6ffe
Hector-bit <hectormartinez.2978@gmail.com>
2022-02-22 07:16:31 -0800

    reroute

--- src/components/projects/FRCard.js
+++ src/components/projects/FRCard.js
@@ -7,7 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import androidGamePicture from '../../images/fr.webp';
-import AndroidGameModal from './AndroidGameModal';
+import AndroidGameModal from './FRModal';
 
 const useStyles = makeStyles({
   root: {
